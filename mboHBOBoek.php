2841699ecd7542fd829b66bd8cd868c7bfd0ffb9
Jack-Kartoush <75987684+Jack-Kartoush@users.noreply.github.com>
2021-01-11 16:21:25 +0100

    Add files via upload

---
+++ mboHBOBoek.php
@@ -0,0 +1,89 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+     <head>
+        <title>bookstromen</title>
+       
+        <link rel="stylesheet" href="../stylesheets/opleiding.css">
+         
+       
+        <!--link van de body fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com"> 
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,500&display=swap" rel="stylesheet"> 
+        <!--eind link -->
+        <!--link title--> 
+        <link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,500&family=Rokkitt:wght@700&display=swap" rel="stylesheet"> 
+<!--eind link-->
+    </head>
+     <body>
+         
+         
+        <header class='header'>
+            <a href='#' class='logo'>L3</a>
+            <input class="menu-btn" type='checkbox' id='menu-btn'/>
+            <label class='menu-icon' for='menu-btn'><span class='nav-icon'></span></label>
+
+
+            <ul class='menu'>
+                 <li><a href="../index.php">Home</a></li>
+                  <li><a href="mboHBOBoek.php">MBO Boeken</a></li>
+                   <li><a href="mboHBOBoek.php">HBO boeken</a></li>
+                   <li><a href="aboutUs.php">About us</a></li>
+                   <li><a href="contact us.php">Contact us</a></li>
+                   <li class="log-in"><a href="inlogAndSignup.php">Log in</a></li>
+                   <li class="sing-up"><a href="inlogAndSignup.php">Sing up</a></li>
+             </ul>
+       </header>
+       <br>
+       <br>
+       <br>
+       
+       
+       <div class='Container'>
+        <div class='InerContainer'>
+              <a href='#' target="_blank">  <img class='boek1' src="../img/Assistent_logistiek.jpg" alt="1Error!! Foto Niet gevonden"></a>
+               <a href='#' target="_blank"><img class='boek2' src="../img/boek2.jpg" alt="2Error!! Foto Niet gevonden"></a>
+            <a href='#' target="_blank"><img class='boek3' src="../img/boek3.jpg" alt="3Error!! Foto Niet gevonden"></a>
+
+                   <a href='#' target="_blank"><img class='boek4' src="../img/boek4.jpg" alt="4Error!! Foto Niet gevonden"></a>
+                <a href='#' target="_blank"><img class='boek5' src="../img/boek5.jpg" alt="5Error!! Foto Niet gevonden"></a>
+                
+          </div>
+</div>
+     <div class='Container2'>
+        <div class='InerContainer2'>
+                 <a href='#' target="_blank"><img class='boek6' src="../img/boek6.jpg" alt="6Error!! Foto Niet gevonden"></a>
+        </div>
+     </div>
+
+
+      
+ 
+           <nav class='tag'>
+              <ul class='tagliste'>
+                 <li ><a href="#">Tag</a></li>
+                 <li ><a href="#">MBO-1</a></li>
+                 <li ><a href="#">MBO-2</a></li>
+                 <li ><a href="#">MBO-3</a></li>
+                 <li  ><a href="#">MBO-4</a></li>
+                 <li ><a href="#">HBO</a></li>
+
+             </ul>
+          </nav>
+
+
+          
+
+ 
+
+
+
+     </body>
+ </html>
